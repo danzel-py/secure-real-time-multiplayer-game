f85f8970757759049ceaeebe59aec5ed2d2a86a2
danzel-py <67574872+danzel-py@users.noreply.github.com>
2021-07-19 21:21:10 +0900

    added functions

--- public/Player.mjs
+++ public/Player.mjs
@@ -1,10 +1,29 @@
 class Player {
-  constructor({x, y, score, id}) {
+  constructor( x, y, score, id ) {
+    this.x = x;
+    this.y = y;
+    this.score = score;
+    this.id = id;
 
   }
 
   movePlayer(dir, speed) {
-
+    switch (dir) {
+      case "up":
+        this.y += speed;
+        break;
+      case "right":
+        this.x += speed;
+        break;
+      case "down":
+        this.y -= speed;
+        break;
+      case "left":
+        this.x -= speed;
+        break;
+      default:
+        break;
+    }
   }
 
   collision(item) {
@@ -12,7 +31,7 @@ class Player {
   }
 
   calculateRank(arr) {
-
+    console.log(arr)
   }
 }
 
